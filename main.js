190020ba9eefddf6d8b88b18517b27766afc3539
Sarfo carbell <kofiking179@gmail.com>
2020-04-21 00:10:07 +0000

    loaders

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function openMenu() {
     navButton.classList.add("close");
     navMenu.classList.add("show");
     navList.classList.add("show");
-    navItem.forEach(item => {
+    navItem.forEach((item) => {
       item.classList.add("show");
     });
 
@@ -20,7 +20,7 @@ function openMenu() {
     navButton.classList.remove("close");
     navMenu.classList.remove("show");
     navList.classList.remove("show");
-    navItem.forEach(item => {
+    navItem.forEach((item) => {
       item.classList.remove("show");
     });
 
@@ -29,7 +29,7 @@ function openMenu() {
 }
 
 // hide menu on click
-navItem.forEach(item => {
+navItem.forEach((item) => {
   item.addEventListener("click", openMenu);
 });
 
@@ -40,13 +40,13 @@ const sectionPortfolio = document.querySelector(".section-portfolio");
 const sectionContact = document.querySelector(".section-contact");
 const homeLink = document.querySelector(".home");
 const options = {
-  rootMargin: "-70px 0px 0px 0px"
+  rootMargin: "-70px 0px 0px 0px",
 };
-const sectionObserver = new IntersectionObserver(function(
+const sectionObserver = new IntersectionObserver(function (
   entries,
   sectionObserver
 ) {
-  entries.forEach(entry => {
+  entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       navMenu.classList.add("nav-scrolled");
       homeLink.classList.remove("current");
@@ -64,13 +64,25 @@ sectionObserver.observe(sectionHome);
 
 // ====== hop =======
 const hopItem = document.querySelectorAll(".m-left");
-hopItem.forEach(item => {
-  item.addEventListener("mouseover", function() {
+hopItem.forEach((item) => {
+  item.addEventListener("mouseover", function () {
     item.classList.add("hop");
   });
 });
-hopItem.forEach(item => {
-  item.addEventListener("mouseout", function() {
+hopItem.forEach((item) => {
+  item.addEventListener("mouseout", function () {
     item.classList.remove("hop");
   });
 });
+
+// ================ loaders ================
+const main = document.querySelector(".main");
+const loaders = document.querySelector(".loaders");
+
+setTimeout(() => {
+  loaders.style.opacity = 0;
+  loaders.style.display = "none";
+
+  main.style.display = "block";
+  main.style.opacity = 1;
+}, 4000);
