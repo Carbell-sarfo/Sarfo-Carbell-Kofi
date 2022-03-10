5eaf6832f29e44449f837b46b35d9acaa3924684
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2022-03-10 17:21:00 +0000

    launching again 2

---
+++ public/js/slick.js
@@ -0,0 +1,8 @@
+$(".testimonials-container").slick({
+    infinite: true,
+    autoplay: true,
+    dots: true,
+    pauseOnDotsHover: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-circle-chevron-left"></i></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-circle-chevron-right"></i></button>'
+  });
