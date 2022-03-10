fb9a62a8de08bc08adae423756b4bac8dc87383f
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2022-03-10 19:04:42 +0000

    cart icon added

--- public/js/slick.js
+++ public/js/slick.js
@@ -4,5 +4,24 @@ $(".testimonials-container").slick({
     dots: true,
     pauseOnDotsHover: true,
     prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-circle-chevron-left"></i></button>',
-    nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-circle-chevron-right"></i></button>'
+    nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-circle-chevron-right"></i></button>',
+
+    responsive: [
+      // {
+      //   breakpoint: 1024,
+      //   settings: {
+      //     slidesToShow: 3,
+      //     slidesToScroll: 3,
+      //     infinite: true,
+      //     dots: true
+      //   }
+      // },
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false
+        }
+      },
+  
+    ]
   });
