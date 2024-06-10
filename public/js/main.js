cb5abdc3a26a3a5e66ba04b14c192abc965220c0
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2024-06-10 19:09:36 +0000

    home page re-arranged

--- public/js/main.js
+++ public/js/main.js
@@ -184,53 +184,53 @@ observer.observe(sectionHero);
 
 
 // 4. ======================== CHANGE TEXT WITH JS ====================
-let i = 0;
+// let i = 0;
 
-const randomizeText = () => {
-  const phrase = document.querySelector('.hero-name');
-  const compStyles = window.getComputedStyle(phrase);
-  const animation = compStyles.getPropertyValue('animation');
-  const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
+// const randomizeText = () => {
+//   const phrase = document.querySelector('.hero-name');
+//   const compStyles = window.getComputedStyle(phrase);
+//   const animation = compStyles.getPropertyValue('animation');
+//   const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
   
-  const phrases = ["Carbell", "Sarfo", "Kofi" ];
+//   const phrases = ["Carbell", "Sarfo", "Kofi" ];
   
-  i = randomNum(i, phrases.length);
-  const newPhrase = phrases[i];
+//   i = randomNum(i, phrases.length);
+//   const newPhrase = phrases[i];
   
-  setTimeout(() => {
-    phrase.textContent = newPhrase;
-  }, 400); // time to allow opacity to hit 0 before changing word
-}
+//   setTimeout(() => {
+//     phrase.textContent = newPhrase;
+//   }, 400); // time to allow opacity to hit 0 before changing word
+// }
 
-const randomNum = (num, max) => {
-  let j = Math.floor(Math.random() * max);
+// const randomNum = (num, max) => {
+//   let j = Math.floor(Math.random() * max);
   
-  // ensure diff num every time
-  if (num === j) {
-    return randomNum(i, max);
-  } else {
-    return j;
-  }
-}
+//   // ensure diff num every time
+//   if (num === j) {
+//     return randomNum(i, max);
+//   } else {
+//     return j;
+//   }
+// }
 
-const getAnimationTime = () => {
-  const phrase = document.querySelector('.hero-name');
-  const compStyles = window.getComputedStyle(phrase);
-  let animation = compStyles.getPropertyValue('animation');
+// const getAnimationTime = () => {
+//   const phrase = document.querySelector('.hero-name');
+//   const compStyles = window.getComputedStyle(phrase);
+//   let animation = compStyles.getPropertyValue('animation');
   
-  // firefox support for non-shorthand property
-  animation != "" ? animation : animation = compStyles.getPropertyValue('-moz-animation-duration');
+//   // firefox support for non-shorthand property
+//   animation != "" ? animation : animation = compStyles.getPropertyValue('-moz-animation-duration');
   
-    // webkit support for non-shorthand property
-  animation != "" ? animation : animation = compStyles.getPropertyValue('-webkit-animation-duration');
+//     // webkit support for non-shorthand property
+//   animation != "" ? animation : animation = compStyles.getPropertyValue('-webkit-animation-duration');
   
   
-  const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
-  return animationTime;
-}
+//   const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
+//   return animationTime;
+// }
 
-randomizeText();
-setInterval(randomizeText, getAnimationTime());
+// randomizeText();
+// setInterval(randomizeText, getAnimationTime());
 
 
 
