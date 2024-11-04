a1710b3d5b9279e9ec446bcfe98156d0782d2945
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2024-11-04 21:31:55 +0000

    development branch now created

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,11 +1,13 @@
 export const mainMenu = [
-     {link:"#", content:"Home", icon:"lucide:home"},
-     {link:"#about", content:"About", icon:"octicon:person-24"},
-     {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
-     {link:"#blog", content:"Blog", icon:"mdi:blog"}
+     {link:"/", content:"Home", icon:"lucide:home"},
+     {link:"/speaking", content:"Speaking", icon:"tabler:microphone-2"},
+     {link:"/about", content:"About", icon:"octicon:person-24"},
+     // {link:"/portfolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"https://www.youtube.com/channel/UCdC_rW9x4MnQvubSJOg4ihQ?sub_confirmation=1", content:"YouTube", icon:"hugeicons:youtube"},
+     {link:"/contact", content:"Contact", icon:"material-symbols:call-outline"},
 ]
 export const menuBlog = [
      {link:"/", content:"Home", icon:"lucide:home"},
-     {link:"/#about", content:"About", icon:"octicon:person-24"},
-     {link:"/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"/about", content:"About", icon:"octicon:person-24"},
+     {link:"/contact", content:"Contact", icon:"material-symbols:call-outline"},
 ]
