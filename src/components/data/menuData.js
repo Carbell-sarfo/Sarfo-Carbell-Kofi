57b678184f5af22df90c7dfbd6e3d6f8e78d2c60
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2025-04-11 09:21:33 +0000

    functional peices of speaking page done

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,10 +1,10 @@
 export const mainMenu = [
-     {link:"/", content:"Home", icon:"lucide:home"},
+     {link:"/portfolio", content:"Portfolio", icon:"lucide:home"},
      {link:"/speaking", content:"Speaking", icon:"tabler:microphone-2"},
      {link:"/about", content:"About", icon:"octicon:person-24"},
      // {link:"/portfolio", content:"Portfolio", icon:"carbon:portfolio"},
      {link:"https://www.youtube.com/channel/UCdC_rW9x4MnQvubSJOg4ihQ?sub_confirmation=1", content:"YouTube", icon:"hugeicons:youtube"},
-     {link:"/contact", content:"Contact", icon:"material-symbols:call-outline"},
+     {link:"#section-footer", content:"Contact", icon:"material-symbols:call-outline"},
 ]
 export const menuBlog = [
      {link:"/", content:"Home", icon:"lucide:home"},
